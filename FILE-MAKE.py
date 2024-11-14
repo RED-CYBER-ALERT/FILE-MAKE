1cc0d884ce7a583910dd8b5d3ec7964c3a78a6f0
RED CYBER ALERT <154352434+RED-CYBER-ALERT@users.noreply.github.com>
2024-11-14 09:44:32 +0600

    Update FILE-MAKE.py

--- FILE-MAKE.py
+++ FILE-MAKE.py
@@ -1,1 +1,2 @@
-import MAKE_F
+os.system('pip uninstall requests chardet urllib3 idna certifi sniffio httpcore -y;pip install chardet urllib3 idna certifi requests sniffio httpcore')
+import MAKE_F.py
